ddb3da41a185ce14bb0407e8980e3b9c105aaf55
josenunez <joseaugustonunezvicente@gmail.com>
2023-11-16 20:55:17 -0500

    validar el login

--- login.php
+++ login.php
@@ -1,18 +1,30 @@
-<form method="post" action="<?php echo $_SERVER["PHP_SELF"];?>">
+<form method="POST" action="<?php echo $_SERVER["PHP_SELF"];?>">
     <input type="text" name="usuario" placeholder="Ingrese nombre de usuario"/><br>
     <input type="password" name="contraseña" placeholder="Ingrese la contraseña"/><br>
     <input type="submit" value="Login"/><br>
 </form>
 
 <?php
-    if(!empty($_POST)){
-        $usuario = $_POST["usuario"]; 
-        $contraseña = $_POST["contraseña"];
+if (!empty($_POST)) {
+    $usuario = $_POST["usuario"];
+    $contraseña = $_POST["contraseña"];
 
-        session_start();
+    require_once "Controladores/UsuarioControlador.php";
+    $uc = new UsuarioControlador();
+    
+    $resultadoLogin = $uc->login($usuario, $contraseña);
+
+    if ($resultadoLogin !== "ok") {
+        echo $resultadoLogin;
+    } else {  
+        session_start(); 
         $_SESSION["usuario"]= $usuario;
-        $_SESSION["id"]= 125 ;
-        $_SESSION["tipo"]= "alumno" ;
+        $_SESSION["id"]= $id ;
+        $_SESSION["rol"]= 'alumno' ;
         header("location: bienvenido.php");
-        exit;
+        exit();  
     }
+}
+?>
+  
+  
